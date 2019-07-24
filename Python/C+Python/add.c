bb3761853ebf2ff16aa0644a7facf5c05a9b9e84
jihan <Mr.JiHan.Mail@gmail.com>
2019-07-24 16:06:25 +0800

    add new example about C + Python

---
+++ Python/C+Python/add.c
@@ -0,0 +1,18 @@
+//sample C file to add 2 numbers - int and floats
+//compile share lib:
+//gcc -shared -Wl,-soname,adder -o adder.so -fPIC add.c
+
+#include <stdio.h>
+
+int add_int(int, int);
+float add_float(float, float);
+
+int add_int(int num1, int num2){
+    return num1 + num2;
+
+}
+
+float add_float(float num1, float num2){
+    return num1 + num2;
+
+}
